f4dcc574779a3a222d731fd4ffbfd59722e0956e
Nick <njhuckov@gmail.com>
2018-10-13 11:07:39 +0300

    - добавлена документация для функции foo()

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import math
 
 
 def foo(x=0, y=0):
+    """
+        Функция, которая считает сумму корней из x
+
+        x - входной аргумент
+        y - начальное значение инкремента
+
+    :param x:
+    :param y:
+    :return float:
+    """
     res = 0  # type: int
 
     while y <= 100:
